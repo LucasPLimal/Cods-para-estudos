dc6ef2fb481678db0b7cb3166e1212b93bd66e14
lucas <pereira.lima1@escolar.ifrn.edu.br>
2024-03-15 08:34:13 -0300

    Idade

---
+++ farofa.js
@@ -0,0 +1,7 @@
+function subtrair() {
+    let a = 2024;
+    let b = document.getElementById("Data").value;
+    ida = a - b;
+    document.getElementById("resultado").innerHTML = 
+        `Idade = ${ida}`;
+}
