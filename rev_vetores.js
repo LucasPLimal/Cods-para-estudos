7ce46f152f2735afb08693de4ba031c57e93da6b
LucasPLimal <pereira.lima1@escoar.ifrn.edu.br>
2024-07-15 08:00:15 -0300

    vetores

---
+++ rev_vetores.js
@@ -0,0 +1,15 @@
+let frutas=["tamarindo", "cajá", "tomate"]
+frutas.push("melão","uva")
+frutas.unshift("acabaxi")
+frutas.splice(2, 0, "uva", "framboesa")
+console.log(frutas.toString())
+
+for (let i=0; i<frutas.length; i++){
+    console.log(frutas[i]);
+}
+
+numero = [2,3,7,8,10]
+console.log(numero.toString())
+
+quadrados = numero.map(function(num){return num ** 2})
+console.log(quadrados.toString())
