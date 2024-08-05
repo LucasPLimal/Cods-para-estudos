89bcfd6cb4e0ecb144fb38f1bbcfa4bd588314a1
LucasPLimal <pereira.lima1@escolar.ifrn.edu.br>
2024-08-05 07:47:47 -0300

    cpp

--- C++/Prática.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-main() {
-    int num;
-    num=10;
-    cout << num << endl;
-    return 0;
-}
---
+++ C++/nota.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+main() {
+    cout << "Situação do aluno" << endl;
+    string aluno;
+    cout << "Nome do Aluno: ";
+    cin >> aluno;
+    cout << "Nota do 1B" << endl;
+    int n1;
+    cin >> n1;
+    cout << "Nota do 2B" << endl;
+    int n2;
+    cin >> n2;
+    cout << "Nota do 2B" << endl;
+    int n3;
+    cin >> n3;
+    return 0;
+}
