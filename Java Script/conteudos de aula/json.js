4631aeb48348117e447c3d38bc682c7f73ab7431
LucasPLimal <pereira.lima1@escolar.ifrn.edu.br>
2024-08-06 08:11:36 -0300

    json

--- Java Script/conteudos de aula/json.js
+++ Java Script/conteudos de aula/json.js
@@ -1,4 +1,4 @@
-//JSON é um objeto com variaveis(atributos) que podem ser alterados como um mockup.
+//JSON é um objeto com variaveis(atributos) que podem ser alteradas como um mockup.
 
 //Ex de objeto em JavaScript:
 const pessoa= {
@@ -9,11 +9,12 @@ const pessoa= {
         return `Ola, meu nome é ${this.nome} tenho ${this.idade} anos e sou ${this.profissão}`;
     }
 }
+
 console.log(pessoa) //Returna o objeto inteiro
 console.log(pessoa.nome) //returna só o nome
 console.log(pessoa.idade) //returna só a idade
-console.log(pessoa.saudação()) //returna a função(método do objeto) saudação
 console.log(pessoa.profissão) //returna só a profissão
+console.log(pessoa.saudação()) //returna a função(método do objeto) saudação
 
 pessoa.idade=31 // altera o atributo "idade" do objeto
 
@@ -21,7 +22,7 @@ pessoa.idade=31 // altera o atributo "idade" do objeto
 console.log("gerando String JSON")
 const strjson=JSON.stringify(pessoa); //stringify para transformar em JSON // parse para destransformar
 console.log(strjson)
-//perceba que no objeto JSON não há a função(saudação) existente no objeto JavaScript
+//perceba que no objeto JSON não há as funções(saudação() e exibir()) existentes no objeto JavaScript
 
 /* 
 Objetos JSON são muito usados em:
