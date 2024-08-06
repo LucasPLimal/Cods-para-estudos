8eed878182b719142da3dd5d0ac6bd528ea5cd43
LucasPLimal <pereira.lima1@escolar.ifrn.edu.br>
2024-08-06 07:23:30 -0300

    no

--- C++/nota.cpp
+++ C++/nota.cpp
@@ -7,19 +7,20 @@ main() {
     cout << "Nome do Aluno: ";
     cin >> aluno;
 
-    cout << "Nota do 1B" << endl;
     int n1;
+    int n2;
+    int n3;
+    
+    cout << "Nota do 1B" << endl;
     cin >> n1;
     cout << "Nota do 2B" << endl;
-    int n2;
     cin >> n2;
-    cout << "Nota do 2B" << endl;
-    int n3;
+    cout << "Nota do 3B" << endl;
     cin >> n3;
 
     float media;
     media=(n1+n2+n3)/3;
-    
+
     if (media>=7) {
         cout << aluno << " Aprovado(a)" << endl;
     }
