982ab5b773a982f7ee38415d8cdd2290d029fb05
LucasPLimal <pereira.lima1@escolar.ifrn.edu.br>
2024-09-04 21:07:36 -0300

    select

--- Java Script/Práticas/exercicios json placeholder/Q3(Trab).js
+++ Java Script/Práticas/exercicios json placeholder/Q3(Trab).js
@@ -1,12 +1,31 @@
-const url='https://worldcupjson.net/teams'
+const url_times = "https://worldcupjson.net/teams";
+fetch(url_times)
+  .then(response => response.json())
+  .then(times => list_teams(times));
 
-fetch(url)
-    .than(response => response.json)
-    .than(json => listar_times(json))
-
-function listar_times(json) {
-    let resultado = document.getElementById("resultado")
-    resultado.innerHTML = ''
-    for (let times of json)
-        resultado.innerHTML= `${times.letra}`
+function list_teams(times) {
+  let select = document.getElementById('country');
+  for (let time of times) {
+    let option = new Option(time.id, time.country);
+    select.options[select.options.length] = option;
+  }
 }
+
+function listar() {
+    const time = document.getElementById('country').value;
+    const url = `https://worldcupjson.net/teams/${time}`;
+    
+    fetch(url)
+        .then(response => response.json())
+        .then(partidas => list_matches(partidas));
+}
+
+function list_matches(partidas) {
+    let totalidade = document.getElementById('totalidade');
+    totalidade.innerHTML = '';
+    for (let partida of partidas) {
+        let item = document.createElement('li');
+        item.innerHTML = partida.status
+        totalidade.appendChild(item)
+    }
+}  
