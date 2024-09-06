300e48c4f651e2e0842e3c650f876fd0d05ed580
LucasPLimal <pereira.lima1@escoar.ifrn.edu.br>
2024-09-06 08:29:39 -0300

    trabul

--- Java Script/Práticas/exercicios json placeholder/Q3(Trab).js
+++ Java Script/Práticas/exercicios json placeholder/Q3(Trab).js
@@ -1,34 +1,38 @@
 const url_teams = 'https://worldcupjson.net/teams';
+const url_matches = 'https://worldcupjson.net/matches'
 fetch(url_teams)
   .then(response => response.json())
   .then(teams => list_teams(teams))
 
 function list_teams(teams) {
   let select = document.getElementById('seleção');
-  for (let team of teams) {
-    let option = new Option(team.country, team.id);
-    select.options[select.options.length] = option;
+  for (let team of teams['groups']) {
+    for (let country of team.teams) {
+      let option = new Option(country.name, country.country);
+      console.log(country.country);
+      select.options[select.options.length] = option;
+    }
   }
 }
 
 function listar() {
     const team = document.getElementById('seleção').value;
-    const url = `https://worldcupjson.net/teams/${team}`;
+    const url = `https://worldcupjson.net/matches`;
     
     fetch(url)
         .then(response => response.json())
         .then(matches => list_matches(matches));
+        
+        function list_matches(matches) {
+          let totalidade = document.getElementById('totalidade');
+          totalidade.innerHTML = '';
+          
+            let descrição = document.createElement('div')
+            descrição.innerHTML = `<p>#${matches.home_team_country}</p>
+                                  <p>Usuário: ${matches.country}</p>
+                                  <p>Título: ${matches.wins}</p>
+                                  <p>Texto: ${matches.losses}</p>`
+            totalidade.appendChild(descrição)
+          }
 }
 
-function list_matches(matches) {
-    let totalidade = document.getElementById('totalidade');
-    totalidade.innerHTML = '';
-    for (let match of matches) {
-      let descrição = document.createElement('div')
-      descrição.innerHTML = `<p>#${match.id}</p>
-                            <p>Usuário: ${match.country}</p>
-                            <p>Título: ${match.wins}</p>
-                            <p>Texto: ${match.losses}</p>`
-      totalidade.appendChild(descrição)
-    }
-}  
