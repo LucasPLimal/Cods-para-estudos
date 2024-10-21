141453cab7329d4c7d24c2e71c67f0f951f4022e
LucasPLimal <pereira.lima1@escolar.ifrn.edu.br>
2024-10-20 22:41:01 -0300

    pokepoke

--- pokemon.js
+++ pokemon.js
@@ -1,21 +1,38 @@
-const url = `https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1302`;
+const url_select = `https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1302`;
 
-fetch(url)
+fetch(url_select)
 .then(response => response.json())
-.then(data => exibir_pokemon(data))
+.then(data=> pokedex(data))
 
-function exibir_pokemon(data) {
+let select = document.getElementById("seleção");
+let img = document.createElement("img");
 
+
+function pokedex(data) {
+
+    for (let poke of data.results) {
+        let option = new Option(poke.name);
+        select.options[select.options.length] = option;
+        
+    }
+}
+
+function listar() {
+    const pokemon = document.getElementById("seleção").value;
+    let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data2 => exibir_pokemon(data2))
+    }
+
+function exibir_pokemon(data2) {
     let resultado = document.getElementById("resultado");
     resultado.innerHTML = '';
-    let img = document.createElement("img");
 
-     for (let poke of data.results) {
-        img.src = poke.url;
-        let descrição = document.createElement("div");
-        descrição.appendChild(img);
-        descrição.innerHTML = `<br>Nome: ${poke.name}<br>
-                                   Imagem: ${poke.url}`
-        resultado.appendChild(descrição)
+    for (let stats of data2) {
+        let descriçao = document.createElement("div")
+        descriçao.innerHTML = ` <br>${stats.name}`
+    resultado.appendChild(descriçao)
     }
 }
