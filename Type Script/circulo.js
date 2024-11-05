720fed6428f73709beeb0ccb9ffbbb66d200181c
LucasPLimal <pereira.lima1@esccolar.ifrn.edu.br>
2024-11-05 11:56:13 -0300

    set

--- Type Script/circulo.js
+++ Type Script/circulo.js
@@ -1,15 +1,37 @@
 var circulo = /** @class */ (function () {
-    function circulo(raio, x, y) {
+    function circulo(name, raio, x, y) {
+        this.name = name;
         this.raio = raio;
         this.x = x;
         this.y = y;
     }
+    circulo.prototype.getRaio = function () {
+        return this.raio;
+    };
+    circulo.prototype.setRaio = function (r) {
+        if (r <= 0)
+            console.log("Ta ficando maluco de colocar o raio igual a 0???? Cadela.");
+        else if (r > 15)
+            console.log("Pode não amigão");
+        else
+            this.raio = r;
+    };
     circulo.prototype.exibir = function () {
-        return "Raio: ".concat(this.raio, ", X: ").concat(this.x, ", Y: ").concat(this.y);
+        return " ".concat(this.name, " = Raio: ").concat(this.raio, ", X: ").concat(this.x, ", Y: ").concat(this.y);
+    };
+    circulo.prototype.mover = function (x, y) {
+        this.x += x;
+        this.y += y;
+    };
+    circulo.prototype.aumentar = function (raio) {
+        this.raio += raio;
     };
     return circulo;
 }());
-var circulo1 = new circulo(2, 0, 0);
-var circulo2 = new circulo(3, 5, 2);
-console.log(circulo1.exibir());
-console.log(circulo2.exibir());
+var c1 = new circulo("circulo1", 2, 0, 0);
+c1.mover(20, 45);
+console.log(c1.exibir());
+var c2 = new circulo("circulo2", 3, 5, 2);
+c2.setRaio(0);
+c2.aumentar(37);
+console.log(c2.exibir());
--- Type Script/circulo.ts
+++ Type Script/circulo.ts
@@ -1,20 +1,47 @@
 class circulo {
-    raio: number;
-    x: number;
-    y: number;
+     private name: string;
+     private raio: number;
+     private x: number;
+     private y: number;
 
-    constructor(raio: number, x: number, y: number) {
+    constructor(name: string, raio: number, x: number, y: number) {
+        this.name = name
         this.raio = raio;
         this.x = x;
         this.y = y;
     }
-    exibir() : string {
-        return `Raio: ${this.raio}, X: ${this.x}, Y: ${this.y}`;   
+
+    getRaio() : number {
+        return this.raio;
+    }
+
+    setRaio(r : number) : void {
+        if(r <= 0)
+            console.log("Ta ficando maluco de colocar o raio igual a 0???? Cadela.")
+        else if(r > 15)
+            console.log("Pode não amigão");
+        else 
+            this.raio = r;
+    }
+
+     public exibir() : string {
+        return ` ${this.name} = Raio: ${this.raio}, X: ${this.x}, Y: ${this.y}`;   
+    }
+     public mover(x: number, y: number) : void {
+        this.x += x;
+        this.y += y;
+    }
+     public aumentar(raio : number) : void {
+        this.raio += raio;
     }
 }
 
-let circulo1 = new circulo(2, 0, 0);
-let circulo2 = new circulo(3, 5, 2);
+let c1 = new circulo("circulo1", 2, 0, 0);
+c1.mover(20, 45);
+console.log(c1.exibir());
 
-console.log(circulo1.exibir())
-console.log(circulo2.exibir())
+let c2 = new circulo("circulo2",3, 5, 2);
+c2.setRaio(0);
+
+c2.aumentar(37);
+console.log(c2.exibir());
